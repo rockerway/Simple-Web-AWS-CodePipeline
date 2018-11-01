031eaf57406b1d36dfefa6ee36e9086366ab4beb
tod.shen <tod.shen@104.com.tw>
2018-11-01 23:40:57 +0800

    chore: dev version 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Simple-Web-AWS-CodePipeline
+
 Simple web CI/CD by Travis CI and AWS CodePipeline
+Language: Go lang
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+const port int = 80
+const message string = "Go Language Server ~ dev 0.0.1"
+
+func main() {
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		log.Println(req.Method + " " + req.URL.String() + " " + req.Proto)
+		fmt.Fprintf(res, message)
+	})
+	http.HandleFunc("/healthcheck", func(res http.ResponseWriter, req *http.Request) {
+		res.WriteHeader(200)
+		res.Write([]byte("ok"))
+	})
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	log.Println("Server has started to listen at port: " + strconv.Itoa(port))
+	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
